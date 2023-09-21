1730599005b9533a5d2f2bfe56cab56af5d326ca
nazi <nazipaeez777@gmail.com>
2023-09-21 20:06:53 +0330

    Create navbar & responsive

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {BiSearchAlt} from 'react-icons/bi'
+import Navbar from './Navbar'
+
+export default function Header() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import Header from '../Components/Header'
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+     <Header/>
+    </div>
   )
 }
